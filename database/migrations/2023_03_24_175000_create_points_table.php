df07f569ef074e282682346e7716b28113bd7bd6
Sadam Alfian <sadamalfianp@gmail.com>
2023-03-25 02:04:28 +0700

    setup trial for migrations and modules

---
+++ database/migrations/2023_03_24_175000_create_points_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->integer('nasabah_id')->unsigned()->index()->nullable();
+            $table->foreign('nasabah_id')->references('id')->on('cms_users')
+                ->onDelete('cascade');
+            $table->integer('total_points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
